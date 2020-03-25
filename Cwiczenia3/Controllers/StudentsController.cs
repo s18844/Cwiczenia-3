04517bc8476d09d8412c44d20bac9dfd753439d3
s18844 <s18844@pjwstk.edu.pl>
2020-03-25 15:09:38 +0100

    Step1
    Pierwszy Etap Zadania

---
+++ Cwiczenia3/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1) return Ok("Kowalski");
+            else if (id == 2) return Ok("Malewski");
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalski,Malewski,Adrzejewski";
+        }
+    }
+}
