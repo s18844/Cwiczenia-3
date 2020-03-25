b35e706e4856130541fc944765866fb59ae8495a
s18844 <s18844@pjwstk.edu.pl>
2020-03-25 15:22:33 +0100

    Metody POST Student
    Dodanie metody ktora po przez POST tworzy Studenta

--- Cwiczenia3/Controllers/StudentsController.cs
+++ Cwiczenia3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cwiczenia3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cwiczenia3.Controllers
@@ -16,6 +17,13 @@ namespace Cwiczenia3.Controllers
             if (id == 1) return Ok("Kowalski");
             else if (id == 2) return Ok("Malewski");
             return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 2000)}";
+            return Ok(student);
         }
 
         [HttpGet]
